189ea63b0ec2df4cd35f5b40eddb78c487d3bbe4
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2023-11-03 16:05:03 -0400

    Arrow Functions

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,13 +1,14 @@
 // require('./js-foundation/03-callbacks')
-const { getUserById } = require('./js-foundation/03-callbacks');
+// const { getUserById } = require('./js-foundation/03-callbacks');
+const { getUserById } = require('./js-foundation/04-arrow');
 
 // getUserById(1);
 
 const id = 1;
 
-getUserById(id, function (error, user) {
+getUserById(id, (error, user) => {
   if (error) {
     throw new Error(error);
   }
-  console.log(user);
+  console.log({ user });
 });
