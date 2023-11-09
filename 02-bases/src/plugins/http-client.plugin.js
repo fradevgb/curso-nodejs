fc6427e35889ec21dbad75a2c5157622fc91a6ed
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2023-11-09 09:49:41 -0400

    Axios - Cliente para peticiones HTTP

--- 02-bases/src/plugins/http-client.plugin.js
+++ 02-bases/src/plugins/http-client.plugin.js
@@ -1,7 +1,11 @@
+const axios = require('axios');
+
 const httpClientPlugin = {
   get: async (url) => {
-    const resp = await fetch(url);
-    return await resp.json();
+    const { data } = await axios.get(url);
+    return data;
+    // const resp = await fetch(url);
+    // return await resp.json();
   },
 
   post: async (url, body) => {},
