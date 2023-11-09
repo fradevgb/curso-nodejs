e2dccc88176bbb0e273b68d49f48c01a278fa8f8
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2023-11-09 08:49:12 -0400

    Promesas

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -3,12 +3,18 @@ const { getAge, getUUID } = require('./plugins');
 // require('./js-foundation/03-callbacks')
 // const { getUserById } = require('./js-foundation/03-callbacks');
 // const { getUserById } = require('./js-foundation/04-arrow');
-const { buildMakePerson } = require('./js-foundation/05-factory');
+// const { buildMakePerson } = require('./js-foundation/05-factory');
+const getPokemonById = require('./js-foundation/06-promises');
 
-const makePerson = buildMakePerson({ getUUID, getAge });
+getPokemonById(4, (pokemon) => {
+  console.log({ pokemon });
+});
 
-const obj = { name: 'John', birthdate: '1985-10-21' };
+// ! Referencia a la función factory y uso
+// const makePerson = buildMakePerson({ getUUID, getAge });
 
-const john = makePerson(obj);
+// const obj = { name: 'John', birthdate: '1985-10-21' };
 
-console.log({ john });
+// const john = makePerson(obj);
+
+// console.log({ john });
